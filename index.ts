ec87dc3a6b254c25d21be721cf3da5de9a5f28eb
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-10-19 07:22:52 +0300

    Update typings

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-import fastifyPlugin = require('fastify-plugin');
-import { ServerResponse } from 'http';
+import fastifyPlugin from 'fastify-plugin';
 
 const ico: string = Buffer.from(
   'AAABAAEAEBACAAEAAQCwAAAAFgAAACgAAAAQAAAAIAAAAAEAAQAAAAAAQAAAAAAAAAAAAAAAAgAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
   'base64'
 ).toString('binary');
 
-function handler(request: FastifyRequest, response: FastifyReply<ServerResponse>): void {
+function handler(request: FastifyRequest, response: FastifyReply): void {
     response.header('Content-Type', 'image/x-icon');
     response.send(ico);
 }
 
-function plugin(fastify: FastifyInstance, options: fastifyPlugin.PluginOptions, next: (err?: Error) => void): void {
+function plugin(fastify: FastifyInstance, options: unknown, next: (err?: Error) => void): void {
     fastify.get('/favicon.ico', handler);
     next();
 }
