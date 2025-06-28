4c571191e7874fc4c0ed1fbd8b34a67bb153527c
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2025-06-28 04:09:40 +0300

    refactor: remove mocha and refactor tests

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const assert = require('node:assert');
 const { equal } = require('node:assert/strict');
+const { after, it } = require('node:test');
 const fastify = require('fastify');
 
-it('GET /favicon.ico returns an empty icon', function (done) {
+it('GET /favicon.ico returns an empty icon', function (_, done) {
   const app = fastify();
   app.register(require('./index'));
 
@@ -15,6 +17,7 @@ it('GET /favicon.ico returns an empty icon', function (done) {
   }, function (err, response) {
     try {
       equal(err, null);
+      assert(response !== undefined);
       equal(response.statusCode, 200);
       equal(response.headers['content-type'], 'image/x-icon');
       equal(response.payload.length, 198);
